28c0139cde5f21ba97fbfae500534c069cb53d53
piasun <77275966+piasun@users.noreply.github.com>
2022-05-01 12:34:31 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -10,7 +10,7 @@ async function getProducts(url) {
 	const products = await response.json();
 	products.forEach(function(product){
 	productContainer.innerHTML += `
-            <a href="../productdetails.html?id=${product.id}" class="product">
+            <a href="products/productdetails.html?id=${product.id}" class="product">
             <h2>${product.name}</h2>
             <div class="product-image" style="background-image:url(${product.images[0].src})"></div>
             </a>`;
